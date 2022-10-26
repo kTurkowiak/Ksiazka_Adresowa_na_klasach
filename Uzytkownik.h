217bfb5594bccfe0cd33a59550c94db4e9105de4
kTurkowiak <kamil.turkowiak.programista@gmail.com>
2022-10-26 11:51:56 +0200

    Create base class KsiazkaAdresowa and class Uzytkownik

---
+++ Uzytkownik.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Uzytkownik
+{
+    int id;
+    string login;
+    string haslo;
+
+
+
+
+
+
+
+
+
+};
--- main.cpp
+++ main.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
 
+#include "KsiazkaAdresowa.h"
+
+using namespace std;
+
+int main()
+{
+    KsiazkaAdresowa ksiazkaAdresowa;
+
+    return 0;
+}
